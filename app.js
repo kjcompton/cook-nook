28b2acc87c1adea3214f4ead38f47dff4c381ca2
Kevin Compton <kevincomptondev@gmail.com>
2023-03-16 18:18:44 -0400

    Set up schema for recipes, Index Route, and seed route

--- app.js
+++ app.js
@@ -1,8 +1,62 @@
 const express = require('express')
-require('dotenv').config()
 const app = express()
+const methodOverride = require('method-override')
+require('dotenv').config()
 const PORT = process.env.PORT
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const mongoURI = 'mongodb://localhost:27017/recipe'
 
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+app.use(express.static('public'));
+
+mongoose.connect(mongoURI, () => {
+    console.log('The connection with mongod is established')
+})
+
+const Recipe = require('./models/recipes.js')
+//HOME
+app.get('/', (req, res) => {
+    res.send('Home')
+})
+//INDEX
+app.get('/recipes', (req, res) => {
+    Recipe.find({}, (err, allRecipes) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send(allRecipes)
+        }
+    })
+})
+//NEW
+
+//DELETE
+
+//UPDATE
+
+//CREATE
+
+//EDIT
+
+//SHOW
+
+//SEED
+const recipeSeed = [
+    {
+        name: 'Test Name',
+        description: 'Test Description'
+    }
+]
+app.get('/recipes/seed', (req, res) => {
+    Recipe.create(recipeSeed, (err, data) => {
+    if (err) console.log(err.message)
+    console.log('Added Recipes')
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
