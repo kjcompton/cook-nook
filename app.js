708fa618646081f8066156403d513bd6e333974e
Kevin Compton <kevincomptondev@gmail.com>
2023-03-16 11:35:50 -0400

    set up gitignore and .env file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
